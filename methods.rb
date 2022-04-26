5942bf354947fcad9d000c0256292d22af032316
Throsby Wells <throsbywells@gmail.com>
2022-04-26 10:19:55 -0400

    commit tree 0e26e31d2dc0091b1ce1563204d0be7ce56a85c8

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(a,b)
+    return a + b
+end
+
+def halve(a)
+    if a.class == Integer
+        return a/2
+    else 
+        return nil
+    end
+end
+
